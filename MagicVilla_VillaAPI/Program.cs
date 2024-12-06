9a70773444d1339ad6454613dba947f83e270cd9
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-12-06 20:06:54 +0000

    configure serilog

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+    .WriteTo.File("log/villaLogs.txt", rollingInterval:RollingInterval.Day).CreateLogger();
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers(option =>
 {

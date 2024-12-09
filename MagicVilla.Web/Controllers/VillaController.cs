e84976c32c5dd851e91f0bd1b9c2bf9a11c41aa0
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-12-09 08:17:56 +0000

    create villa ui

--- MagicVilla.Web/Controllers/VillaController.cs
+++ MagicVilla.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using MagicVilla_Utility;
 using MagicVilla.Web.Models;
 using MagicVilla.Web.Models.Dto.Villa;
 using MagicVilla.Web.Services.IServices;
@@ -28,5 +29,29 @@ public class VillaController : Controller
             list = JsonConvert.DeserializeObject<List<VillaDto>>(Convert.ToString(response.Result));
         }
         return View(list);
+    }
+    
+    
+    public async Task<IActionResult> CreateVilla()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateVilla(VillaCreateDto model)
+    {
+        if (ModelState.IsValid)
+        {
+
+            var response = await _villaService.CreateAsync<ApiResponse>(model);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Villa created successfully";
+                return RedirectToAction(nameof(IndexVilla));
+            }
+        }
+        TempData["error"] = "Error encountered.";
+        return View(model);
     }
 }

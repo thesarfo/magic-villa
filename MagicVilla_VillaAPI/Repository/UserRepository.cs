dec4262875346993568266d440871b95ae7cd739
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-12-10 10:06:00 +0000

    implement token generation on login

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -1,17 +1,23 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto.Users;
 using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MagicVilla_VillaAPI.Repository;
 
 public class UserRepository : IUserRepository
 {
     private readonly ApplicationDbContext _db;
+    private string _secretKey;
 
-    public UserRepository(ApplicationDbContext db)
+    public UserRepository(ApplicationDbContext db, IConfiguration configuration)
     {
         _db = db;
+        _secretKey = configuration.GetValue<string>("ApiSettings:Secret");
     }
 
     public bool IsUniqueUser(string username)
@@ -26,9 +32,38 @@ public class UserRepository : IUserRepository
         return false;
     }
 
-    public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+    public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
     {
-        throw new NotImplementedException();
+        var user = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower()
+        && u.Password == loginRequestDto.Password);
+
+        if (user == null)
+        {
+            return null;
+        }
+        
+        // generate jwt if user was found
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_secretKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role)
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        LoginResponseDto loginResponseDto = new LoginResponseDto()
+        {
+            Token = tokenHandler.WriteToken(token),
+            User = user
+        };
+        return loginResponseDto;
     }
 
     public async Task<LocalUser> Register(RegistrationRequestDto registrationRequestDto)

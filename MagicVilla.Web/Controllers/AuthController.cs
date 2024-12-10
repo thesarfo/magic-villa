7c2d66f2860861bf3e7f374af0435480d52ba96a
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-12-10 16:34:14 +0000

    setup auth controllers and views

---
+++ MagicVilla.Web/Controllers/AuthController.cs
@@ -0,0 +1,88 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using MagicVilla.Web.Models;
+using MagicVilla.Web.Models.Dto.User;
+using MagicVilla.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicVilla.Web.Controllers;
+
+public class AuthController : Controller
+{
+     private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ApiResponse response = await _authService.LoginAsync<ApiResponse>(obj);
+            if (response != null && response.IsSuccess)
+            { 
+                LoginResponseDto model = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(response.Result));
+
+                var handler = new JwtSecurityTokenHandler();
+                var jwt = handler.ReadJwtToken(model.Token);
+
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                identity.AddClaim(new Claim(ClaimTypes.Name, jwt.Claims.FirstOrDefault(u => u.Type == "name").Value));
+                identity.AddClaim(new Claim(ClaimTypes.Role, jwt.Claims.FirstOrDefault(u=>u.Type=="role").Value));
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+
+                //HttpContext.Session.SetString(SD.SessionToken, model.Token);
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", response.ErrorMessages.FirstOrDefault());
+                return View(obj);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ApiResponse result =  await _authService.RegisterAsync<ApiResponse>(obj);
+            if (result != null && result.IsSuccess)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+
+        // public async Task<IActionResult> Logout()
+        // {
+        //     await HttpContext.SignOutAsync();
+        //     HttpContext.Session.SetString(SD.SessionToken, "");
+        //     return RedirectToAction("Index","Home");
+        // }
+        //
+        // public IActionResult AccessDenied()
+        // {
+        //     return View();
+        // }
+}
